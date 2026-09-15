9bd9b7cba4da265dce9c0750fc2147cb8b2ad45e
Jason <jpallen.0628@gmail.com>
2026-09-15 02:21:45 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: { port: 5173, strictPort: true, watch: { usePolling: true, interval: 200, ignored: ['**/artifacts/**', '**/docs/**', '**/dist/**', '**/scripts/**', '**/tests/**'] } },
-  build: { target: 'es2022', chunkSizeWarningLimit: 1800 },
+  base: '/Holo/',
+
+  server: {
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 200,
+      ignored: [
+        '**/artifacts/**',
+        '**/docs/**',
+        '**/dist/**',
+        '**/scripts/**',
+        '**/tests/**'
+      ]
+    }
+  },
+
+  build: {
+    target: 'es2022',
+    chunkSizeWarningLimit: 1800
+  },
 });
